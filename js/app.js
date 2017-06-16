65873ca1fa5dc74c09ec627d61cabc7c6c0f8a41
Patrick Flores <patrickflores.dev@gmail.com>
2017-06-16 17:33:20 +0800

    Added Icomoon Fonts, Search Overlay Layout, Menu Revision, and Footer Logo Optimization

--- js/app.js
+++ js/app.js
@@ -1,41 +1,85 @@
 $(document).foundation();
 (function() {
-	var triggerBttn = document.getElementById( 'trigger-overlay' ),
-		overlay = document.querySelector( 'div.overlay' ),
-		closeBttn = overlay.querySelector( '.overlay-close' );
-		transEndEventNames = {
-			'WebkitTransition': 'webkitTransitionEnd',
-			'MozTransition': 'transitionend',
-			'OTransition': 'oTransitionEnd',
-			'msTransition': 'MSTransitionEnd',
-			'transition': 'transitionend'
-		},
-		transEndEventName = transEndEventNames[ Modernizr.prefixed( 'transition' ) ],
-		support = { transitions : Modernizr.csstransitions };
+    var triggerBttn = document.getElementById('trigger-overlay'),
+        overlay = document.querySelector('div.overlay'),
+        closeBttn = overlay.querySelector('.menu-button');
+    transEndEventNames = {
+            'WebkitTransition': 'webkitTransitionEnd',
+            'MozTransition': 'transitionend',
+            'OTransition': 'oTransitionEnd',
+            'msTransition': 'MSTransitionEnd',
+            'transition': 'transitionend'
+        },
+        transEndEventName = transEndEventNames[Modernizr.prefixed('transition')],
+        support = { transitions: Modernizr.csstransitions };
 
-	function toggleOverlay() {
-		if( classie.has( overlay, 'open' ) ) {
-			classie.remove( overlay, 'open' );
-			classie.add( overlay, 'close' );
-			var onEndTransitionFn = function( ev ) {
-				if( support.transitions ) {
-					if( ev.propertyName !== 'visibility' ) return;
-					this.removeEventListener( transEndEventName, onEndTransitionFn );
-				}
-				classie.remove( overlay, 'close' );
-			};
-			if( support.transitions ) {
-				overlay.addEventListener( transEndEventName, onEndTransitionFn );
-			}
-			else {
-				onEndTransitionFn();
-			}
-		}
-		else if( !classie.has( overlay, 'close' ) ) {
-			classie.add( overlay, 'open' );
-		}
-	}
+    var morphSearch = document.getElementById('morphsearch'),
+        input = morphSearch.querySelector('input.morphsearch-input'),
+        ctrlClose = morphSearch.querySelector('span.morphsearch-close'),
+        isOpen = isAnimating = false,
+        toggleSearch = function(evt) {
+            if (evt.type.toLowerCase() === 'focus' && isOpen) return false;
 
-	triggerBttn.addEventListener( 'click', toggleOverlay );
-	closeBttn.addEventListener( 'click', toggleOverlay );
+            var offsets = morphsearch.getBoundingClientRect();
+            if (isOpen) {
+                classie.remove(morphSearch, 'open');
+                if (input.value !== '') {
+                    setTimeout(function() {
+                        classie.add(morphSearch, 'hideInput');
+                        setTimeout(function() {
+                            classie.remove(morphSearch, 'hideInput');
+                            input.value = '';
+                        }, 300);
+                    }, 500);
+                }
+
+                input.blur();
+            } else {
+                classie.add(morphSearch, 'open');
+            }
+            isOpen = !isOpen;
+        };
+
+    function toggleOverlay() {
+        if (classie.has(overlay, 'open')) {
+            classie.remove(overlay, 'open');
+            //classie.add(overlay, 'close');
+            var onEndTransitionFn = function(ev) {
+                if (support.transitions) {
+                    if (ev.propertyName !== 'visibility') return;
+                    this.removeEventListener(transEndEventName, onEndTransitionFn);
+                }
+                classie.remove(overlay, 'close');
+            };
+            if (support.transitions) {
+                overlay.addEventListener(transEndEventName, onEndTransitionFn);
+            } else {
+                onEndTransitionFn();
+            }
+        } else if (!classie.has(overlay, 'close')) {
+            classie.add(overlay, 'open');
+        }
+    }
+
+    triggerBttn.addEventListener('click', toggleOverlay);
+    closeBttn.addEventListener('click', toggleOverlay);
+    input.addEventListener('focus', toggleSearch);
+    ctrlClose.addEventListener('click', toggleSearch);
+
+    document.addEventListener('keydown', function(ev) {
+        var keyCode = ev.keyCode || ev.which;
+        if (keyCode === 27 && isOpen) {
+            toggleSearch(ev);
+        }
+    });
+
+    $('.trigger.menu-button').click(function() {
+        $(this).addClass('open');
+        $('.overlay.overlay-hugeinc .menu-button').addClass('open');
+    });
+    $('.overlay.overlay-hugeinc .menu-button').on('click', function() {
+        $('.trigger.menu-button').removeClass('open');
+        $(this).removeClass('open');
+    });
+
 })();
